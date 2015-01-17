10cc795521fac998476712a16c36bd2733581a95
Jess Fortier <tjfortier@gmail.com>
2015-01-16 18:14:42 -0800

    Added specs for data persistence during workflow
    Need to check valid state (clearing or saving) both when navigating
    within the app and when returning to the app externally.

--- tests/flow-spec.js
+++ tests/flow-spec.js
@@ -43,4 +43,83 @@ describe('delete button', function() {
 		expect(entry.readName()).toBe('');
 		expect(entry.readPhone()).toBe('');
 	});
+
+	it('clears the data store when clicked', function() {
+		//symptom test, as we're not directly examining the data store
+
+		entry = entry
+			.enterName(validName)
+			.enterPhone(validPhone)
+			.clickSave()
+			.clickBack()
+			.clickDelete();
+
+		entry = ui.go(); //reload the page, prompting prepopulation of fields
+
+		expect(entry.readName()).toBe('');
+		expect(entry.readPhone()).toBe('');
+
+	});
+});
+
+describe('confirmation view', function() {
+	var validName = require('./test-data.js').nameField.valid['is alpha only'];
+	var validPhone = require('./test-data.js').phoneField.valid['contains 10 digits'];
+	var ui;
+	var entry;
+	var confirmation;
+
+	beforeEach(function() {
+		ui = require('../model/BasicPhonebookUI.js');
+		entry = ui.go().ensureNoSavedData();
+	});
+
+	afterEach(function() {
+		entry = ui.go().ensureNoSavedData();
+	});
+
+	it('replaces form fields on successful save', function() {
+		var confirmation = entry
+			.enterName(validName)
+			.enterPhone(validPhone)
+			.clickSave();
+
+		expect(entry.nameFieldShown()).not.toBeTruthy();
+		expect(entry.phoneFieldShown()).not.toBeTruthy();
+	});
+});
+
+describe('entry view', function() {
+	var validName = require('./test-data.js').nameField.valid['is alpha only'];
+	var validPhone = require('./test-data.js').phoneField.valid['contains 10 digits'];
+	var ui;
+	var entry;
+	var confirmation;
+
+	beforeEach(function() {
+		ui = require('../model/BasicPhonebookUI.js');
+		entry = ui.go()
+			.ensureNoSavedData()
+			.enterName(validName)
+			.enterPhone(validPhone)
+			.clickSave()
+			.clickBack();
+	});
+
+	afterEach(function() {
+		entry = ui.go().ensureNoSavedData();
+	});
+
+	it('displays saved data when returning from the confirmation view', function() {
+		expect(entry.readName()).toBe(validName);
+		expect(entry.readPhone()).toBe(validPhone);
+	});
+
+	it('displays saved data when reloading the page', function() {
+		entry.clearFields(); //clear the fields
+		entry = ui.go(); //reload the page to hopefully force prepopulation
+
+		expect(entry.readName()).toBe(validName);
+		expect(entry.readPhone()).toBe(validPhone);
+	});
 });
